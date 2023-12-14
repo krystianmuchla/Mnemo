97dc2884777301e776a2682851ddd6f03dbcb9bc
Krystian Muchla <kmuchla69@gmail.com>
2023-12-14 23:16:06 +0100

    Initial commit

---
+++ app/src/main/java/com/github/krystianmuchla/mnemo/ui/main/MainFragment.kt
@@ -0,0 +1,26 @@
+package com.github.krystianmuchla.mnemo.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.krystianmuchla.mnemo.R
+
+class MainFragment : Fragment() {
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
